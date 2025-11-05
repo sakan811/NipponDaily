147a16ac6f7f84bdfe0ea55924072b5c62524343
Sakan <sakanbeer@gmail.com>
2025-11-05 10:54:01 +0700

    refactor: update Vue components and Tailwind styling

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="min-h-screen bg-gradient-to-br from-(--color-yuki) to-(--color-mizu)">
+  <div
+    class="min-h-screen bg-gradient-to-br from-(--color-yuki) to-(--color-mizu)"
+  >
     <NuxtRouteAnnouncer />
     <JapanNewsReader />
   </div>
